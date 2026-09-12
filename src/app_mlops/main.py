eac70589421d991e6b9a2a28c0eb360e9fd4e291
William Ferreira <33575357+illiamw@users.noreply.github.com>
2026-09-12 18:34:25 -0300

    Teste fluxo ci/cd

--- src/app_mlops/main.py
+++ src/app_mlops/main.py
@@ -93,7 +93,7 @@ def root():
     """
     Health check endpoint.
     """
-    return {"status": "ok"}
+    return {"status": "ok_v0"}
 
 
 # ============================================================
